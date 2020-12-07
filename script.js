fa00d3c7a0792a5147a08b579c0337ae818b55dd
Lauren Dove <elledoveinc@gmail.com>
2020-12-06 21:34:43 -0500

    added the logic to allow circles to be highlighted when selected. Completed

---
+++ script.js
@@ -0,0 +1,62 @@
+const progress = document.getElementById('progress');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+//We must put .circle in this querySelectorAll to grab all of them and it will create a nodeList.
+//Without it, the NodeList will return 0.
+const circles = document.querySelectorAll('.circle');
+
+//console.log(circle)
+
+let currentActive = 1;
+
+
+next.addEventListener('click', () => {
+    currentActive++
+    if( currentActive > circles.length ) {
+        currentActive = circles.length
+    }
+    console.log(currentActive)
+    update()
+})
+
+
+prev.addEventListener('click', () => {
+    currentActive--
+    if( currentActive < 1 ) {
+        currentActive = 1
+    }
+    console.log(currentActive)
+    update()
+})
+
+
+
+
+function update(){
+    circles.forEach((circle,idx) => {
+        if(idx < currentActive){
+            circle.classList.add('active')
+        } else{
+            circle.classList.remove('active')
+        }
+
+       
+    })
+
+    const actives = document.querySelectorAll('.active');
+
+    progress.style.width = (actives.length -1) / (circles.length -1) * 100 + '%'
+
+
+    if(currentActive === 1){
+        prev.disabled = true
+
+    } else if(currentActive === circles.length){
+        next.disabled = true
+
+    } else{
+        prev.disabled = false
+        next.disabled = false
+
+    }
+}
